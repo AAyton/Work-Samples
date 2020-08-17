056251c132f1c257ef0ab419c58edf89183c6ae8
AAyton <62776279+AAyton@users.noreply.github.com>
2020-08-17 13:37:59 +1000

    Added Dialogue Classes

---
+++ DialogueManager.cs
@@ -0,0 +1,129 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueManager : MonoBehaviour
+{
+    [SerializeField]
+    private Text _dialogue;
+    
+    private Text[] _choiceText;
+    private DialogueContainer _currentContainer;
+    private List<NodeLinkData> _link;
+    private List<ExposedProperty> _conditions;
+    private List<DialogueNodeData> _textData;
+    private List<NodeLinkData> _currentChoices;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+   public void OpenDialogue(DialogueContainer dialogueContainer, Text[] choices)
+    {
+        _currentContainer = dialogueContainer;
+        _link = _currentContainer.NodeLinks;
+        _conditions = _currentContainer.ExposedProperties;
+        _textData = _currentContainer.DialogueNodeData;
+        _choiceText = choices;
+       
+
+        _currentChoices = new List<NodeLinkData>();
+
+        int listSize = _textData.Count;
+        // Dialogue.text = textData[listSize - 1].DialogueText;
+        _dialogue.text = _textData[0].DialogueText;
+        // updateChoices(textData[listSize - 1].NodeGUID);
+        updateChoices(_textData[0].NodeGUID);
+    }
+
+     void updateChoices(string baseNodeGuid)
+    {
+        // choices should be cleared before adding new chocies
+        clearChoices();
+        bool validChoice = true;
+        //Update choice text to show new available choices
+        for (int i = 0; i < _link.Count; i++)
+        {
+
+            //for all local link nodes 
+            if (_link[i].BaseNodeGUID == baseNodeGuid)
+            {
+                NodeLinkData targetLink = _link[i];
+                //if it has lock check
+                if (targetLink.lockChecks != null)
+                {
+                    //for all lock checks
+                    for (int a = 0; a < targetLink.lockChecks.Length; a++)
+                    {
+                        //for all nodes in container targeted by lock check
+                        for (int b = 0; b < targetLink.lockChecks[a].targetContainer.NodeLinks.Count; b++)
+                        {
+
+                            //check if node is the required node
+                            if (targetLink.lockChecks[a].targetContainer.NodeLinks[b].PortName == targetLink.lockChecks[a].CheckNodePortName)
+                            {
+                                //check that required node have been used and no forbidden nodes have been used
+                                if (targetLink.lockChecks[a].targetContainer.NodeLinks[b].used != targetLink.lockChecks[a].used)
+                                {
+                                    validChoice = false;
+                                }
+                            }
+                        }
+                    }
+                }
+
+
+
+
+                if (validChoice)
+                {
+                    _currentChoices.Add(_link[i]);
+                }
+            }
+        }
+
+
+        for (int i = 0; i < _currentChoices.Count; i++)
+        {
+
+            _choiceText[i].text = _currentChoices[i].PortName;
+        }
+    }
+
+    public void clearChoices()
+    {
+        _currentChoices.Clear();
+        for (int i = 0; i < _choiceText.Length; i++)
+        {
+            _choiceText[i].text = "";
+        }
+    }
+
+    public void choiceInput(int tempChoice)
+    {
+
+        NodeLinkData choice = _currentChoices[tempChoice];
+
+        //Go through lock targets and update lock and disable status
+        choice.used = true;
+
+
+        //Update text display to show new target node
+        for (int i = 0; i < _textData.Count; i++)
+        {
+
+            if (_textData[i].NodeGUID == choice.TargetNodeGuid)
+            {
+                _dialogue.text = _textData[i].DialogueText;
+                Debug.Log(_dialogue.text);
+               
+                updateChoices(_textData[i].NodeGUID);
+            }
+        }
+
+
+    }
+}
---
+++ DialogueMenu.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueMenu : MonoBehaviour
+{
+
+    [SerializeField]
+    private Button[] _btnChoices;
+
+    [SerializeField]
+    private Button _btnLeaveCharacter;
+
+    [SerializeField]
+    private DialogueManager _dialogueManager;
+
+    [SerializeField]
+    private Canvas _buildingCanvas;
+
+    private DialogueContainer _currentContainer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    public void open(DialogueContainer newContainer)
+    {
+        _currentContainer = newContainer;
+
+        for (int a = 0; a < _btnChoices.Length; a++)
+        {
+            _btnChoices[a].onClick.RemoveAllListeners();
+        }
+        _btnLeaveCharacter.onClick.RemoveAllListeners();
+
+        
+        _btnLeaveCharacter.onClick.AddListener(() => leaveCharacter());
+
+        if (_btnChoices != null)
+        {
+            for (int a = 0; a < _btnChoices.Length; a++)
+            {
+                // passing a directly into choiceInput updates the listener as a increments
+                int characterIndex = a;
+
+                _btnChoices[a].onClick.AddListener(() => _dialogueManager.choiceInput(characterIndex));
+
+            }
+        }
+
+
+        Text[] choices = new Text[_btnChoices.Length];
+
+        for (int i = 0; i < choices.Length; i++)
+        {
+             choices[i] = _btnChoices[i].gameObject.GetComponentInChildren<Text>();
+        }
+
+        _dialogueManager.OpenDialogue(_currentContainer,choices);
+    }
+
+    void leaveCharacter()
+    {
+        GetComponent<Canvas>().gameObject.SetActive(false);
+        _dialogueManager.clearChoices();
+        _buildingCanvas.gameObject.SetActive(true);
+
+    }
+}
